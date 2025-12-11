7ff1d6d7491d15091b652ee3e5f2ea79c528f133
Kim Viktoria <v.kim@tha.kz>
2025-12-11 22:13:04 +0500

    new security conf

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,7 +13,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import java.util.List;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
@@ -33,7 +31,8 @@ public class SecurityConfig {
 
                     c.setAllowedOriginPatterns(List.of(
                             "http://localhost:5173",
-                            "https://fullstack-frontend-demo-delta.vercel.app"
+                            "https://fullstack-frontend-demo-delta.vercel.app",
+                            "https://fullstack-frontend-demo-git-master-kimchishkavs-projects.vercel.app"
                     ));
 
                     c.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
